d94eb5ba73e09a8c7e6967738bd7757cc2b1552e
salar <salaradel12@icloud.com>
2025-10-26 17:20:53 +0200

    just fix

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-unite = input("give me the unite C/F: ")
-temp = input("give me the temp: ")
-#salar is here
+unit = input("What is the unit? C/F: ")
+temp = float(input("What is the temperature? "))
+
+if unit == "C":
+    temp = round((temp * 9) / 5 + 32, 1)
+    print(f"The temperature in F: {temp}")
+
+elif unit == "F":
+    temp = round((temp - 32) * 5 / 9, 1)
+    print(f"The temperature in C: {temp}")
+
+else:
+    print("Please enter a valid unit")
