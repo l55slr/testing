a6f44de9636788c773b8bea1db05fe72ffebde63
salar <salaradel12@icloud.com>
2025-10-26 17:55:23 +0200

    the logical od the "or"

---
+++ logicals.py
@@ -0,0 +1,14 @@
+x = 5
+y = 10
+if x > 100 or y < 10 :
+    print("the condition is valid")
+else:
+    print("the condition is invalid")
+
+
+temp = 20
+raining = False
+if temp < 30 or raining:
+    print("we can go")
+else:
+    print("we can't go")
