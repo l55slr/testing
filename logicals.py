128e7a0c57ca1d6afdd0dc122f6e113833d79104
salar <salaradel12@icloud.com>
2025-10-27 19:02:10 +0200

    the end of logicals

--- logicals.py
+++ logicals.py
@@ -1,3 +1,4 @@
+#The "or" logical, for this logical one condition must be true
 x = 5
 y = 10
 if x > 100 or y < 10 :
@@ -12,3 +13,19 @@ if temp < 30 or raining:
     print("we can go")
 else:
     print("we can't go")
+
+#the "and" logical, both condition must be true
+age = 18
+has_id = False
+vip = True
+
+if (age >= 18 and has_id) or vip:
+    print("we can go")
+else:
+    print("we can not go")
+
+
+
+
+
+
