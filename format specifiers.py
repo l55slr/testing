e7f21fd878f98ba398ed13f6360071035307bd46
salar <salaradel12@icloud.com>
2025-10-31 14:11:01 +0200

    format specifiers = {value:flags}

---
+++ format specifiers.py
@@ -0,0 +1,13 @@
+# the format to limit dicamail numbers after the number: print(f"price1 is {price1:.2f}")
+# the format to add dicamail numbers before the number: print(f"price1 is {price1:10}")
+
+#format specifiers = {value:flags} format a value based on what
+#flags are inserted
+
+
+price1 = 3.1183
+price2 = -32.817
+price3 =12.83
+print(f"price1 is {price1:.2f}")
+print(f"price2 is {price2:10}")
+print(f"price3 is {price3}")
