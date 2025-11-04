08a96169b0498f2bde4a29126f3866b816c6beb3
salar <salaradel12@icloud.com>
2025-11-04 12:54:01 +0200

    Better Bmi version

--- bmi.py
+++ bmi.py
@@ -1,4 +1,16 @@
 mass = float(input("put ur mass in KG"))
 height = float(input("put ur height meters "))
 bmi = mass / (height **2)
-print(bmi)
+print(f"Your BMI is {round(bmi,2)}")
+
+if bmi < 18.5:
+    print("you are underweight")
+elif bmi < 25:
+    print("you are normal")
+elif bmi < 30:
+    print("you are overweight")
+else :
+    print("you are obese")
+
+
+
