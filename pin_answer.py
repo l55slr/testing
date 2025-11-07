22adabd3e3ee57a2d3b1a2f85f2ce9fdc48eacec
salar <salaradel12@icloud.com>
2025-11-07 22:32:44 +0200

    with the "and" operator

--- pin_answer.py
+++ pin_answer.py
@@ -5,3 +5,14 @@ while pin != 1234:
     pin = int(input("ur pin is not right. plz enter ur correct pin: "))
 if pin == 1234:
     print("Your pin is correct")
+
+#<---- with the and operator ---->
+guess = 0
+tries = 0
+while guess != 6 and tries < 5:
+  guess = int(input("guess the number in my mind: "))
+  tries += 1
+if guess != 6:
+    print("Your got the limit")
+else:
+    print("You are smart")
