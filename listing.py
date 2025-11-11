e54eecba7872d665053b315744f5310e3284970b
salar <salaradel12@icloud.com>
2025-11-11 11:59:00 +0200

    the way of how listing work

---
+++ listing.py
@@ -0,0 +1,11 @@
+# for the listing it will be like this
+vowels = ['a', 'e', 'i', 'o', 'u']
+# if I want to get one of them I will
+print(vowels[0])
+# The list or the index of the list start with "0" or u can use (-)
+
+#Other method we can use is (slicing) its the to get more than one indivigual at the same time
+vowles = ['a', 'e', 'i', 'o', 'u']
+print(vowels[0 : 4])
+
+#if you put number out of the range it will display (index error)
