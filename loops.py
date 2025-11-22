126e4e47d8fd06cad7bb7600ff94271783e13d61
salar <salaradel12@icloud.com>
2025-11-22 13:34:25 +0200

    Snake eyes!

--- loops.py
+++ loops.py
@@ -1,3 +1,17 @@
 for i in range (10, 0, -1):
   print(i)
 print("Happy New Year! 🥳")
+
+print("<============================>")
+import random
+die1 = random.randint(1, 6)
+die2 = random.randint(1, 6)
+total = die1 + die2
+
+while total != 2:
+    print("Nope")
+    die1 = random.randint(1, 6)
+    die2 = random.randint(1, 6)
+    total = die1 + die2
+else:
+    print("Snake eyes!")
